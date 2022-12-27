5a070e3c636a63aedc93bcdfdf3b938ad84ae0dd
Link Lee <61494324+jelink27@users.noreply.github.com>
2022-12-27 19:16:45 +0800

    Update pandas_practice.py

--- pandas_practice.py
+++ pandas_practice.py
@@ -4,6 +4,7 @@ movie_ratings = [9.0, 8.9, 8.8, 8.7]
 movie_titles = ["The Dark Knight", "Schindler's List", "Forrest Gump", "Inception"]
 
 #create Dataframe
+#1
 
 data = pd.DataFrame()
 data['movie_titles'] = movie_titles
@@ -15,7 +16,25 @@ print(data['movie_ratings'])
 print(data.columns)
 print(type(data['movie_titles']))
 
+#2
+movie_dict = {
+    "The Dark Knight" : 9.0,
+    "Schindler's List" : 8.9,
+    "Forrest Gump" : 8.8,
+    "Inception" : 8.7
+}
+index_of_data2 = pd.Index([0,1,2,3])
+data2 = pd.DataFrame(movie_dict,index=index_of_data2)
+print(data2)
 
+
+
+movie_dict = {
+    "title": ["The Dark Knight", "Schindler's List", "Forrest Gump", "Inception"],
+    "rating": [9.0, 8.9, 8.8, 8.7]
+}
+df = pd.DataFrame(movie_dict)
+print(df)
 
 #index
 
