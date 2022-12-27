768e4ecb6b19f5cc1b40e2d3fce3bb72a09be363
Link Lee <61494324+jelink27@users.noreply.github.com>
2022-12-27 19:52:16 +0800

    Create loc_iloc.py

---
+++ pandas/loc_iloc.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+movie_ratings = [9.0, 8.9, 8.8, 8.7]
+movie_titles = ["The Dark Knight", "Schindler's List", "Forrest Gump", "Inception"]
+release_years = [2008, 1993, 1994, 2010]
+df = pd.DataFrame()
+df["rating"] = movie_ratings
+df["release_year"] = release_years
+df.index = movie_titles
+df.loc[["The Dark Knight", "Inception"], ["rating", "release_year"]] #只找前面兩部的rating跟release_year loc寫法為row,column
+
+
+print(df[df['release_year'] > 2000]) #也適用華麗索引跟廣播 可用來當遮罩
